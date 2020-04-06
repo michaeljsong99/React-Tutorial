e2a6bdbad3caec26c1f9f96b39fa1d0146108484
Michael Song <mjysong@gmail.com>
2020-04-06 13:09:09 -0400

    Updates

---
+++ precache-manifest.a6cf8a09d014604d0629dcfbdba96672.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbebf08864cd24f768400a79f6e62174",
+    "url": "/React-Tutorial/index.html"
+  },
+  {
+    "revision": "6e1b6f5c37dd4a80460a",
+    "url": "/React-Tutorial/static/css/main.2eecb332.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/React-Tutorial/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Tutorial/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e1b6f5c37dd4a80460a",
+    "url": "/React-Tutorial/static/js/main.b1485b22.chunk.js"
+  },
+  {
+    "revision": "d79a235715b3594b62a3",
+    "url": "/React-Tutorial/static/js/runtime-main.00ca9047.js"
+  }
+]);
